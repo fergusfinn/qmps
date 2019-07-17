bd0bae86c4408760fd7f13b0522ca19db13b7a69
James Dborin <james.dborin.18@ucl.ac.uk>
2019-07-17 12:16:09 +0100

    added vert and horiz swap test to represent

--- qmps/represent.py
+++ qmps/represent.py
@@ -1,8 +1,10 @@
-from .tools import cT, direct_sum, unitary_extension
-
+from .tools import cT, direct_sum, unitary_extension, Optimizer, cirq_qubits, log2, split_2s
+from typing import List, Callable, Dict
 from numpy import concatenate, allclose, tensordot, swapaxes
 from scipy.linalg import null_space, norm
 import cirq
+import numpy as np
+
 
 def sampled_tomography_env_objective_function(U, V, reps=10000):
     """sampled_environment_objective_function: return norm of difference of (sampled) bloch vectors
@@ -31,6 +33,7 @@ def sampled_tomography_env_objective_function(U, V, reps=10000):
     LHS = sampled_bloch_vector_of(qbs[0], LHS, reps)
     RHS = sampled_bloch_vector_of(qbs[0], RHS, reps)
     return norm(LHS-RHS)
+
 
 def full_tomography_env_objective_function(U, V):
     """full_environment_objective_function: return norm of difference of bloch vectors
@@ -61,8 +64,9 @@ def full_tomography_env_objective_function(U, V):
     RHS = sim.simulate(RHS).bloch_vector_of(qbs[0])
     return norm(LHS-RHS)
 
+
 class State(cirq.Gate):
-    def __init__(self, u: ShallowStateTensor, v: ShallowEnvironment, n: int):
+    def __init__(self, u, v, n: int):
         self.u = u
         self.v = v
         self.n_phys_qubits = n
@@ -76,3 +80,188 @@ class State(cirq.Gate):
 
     def num_qubits(self):
         return self.n_phys_qubits + self.v.num_qubits()
+
+
+class ShallowStateTensor(cirq.Gate):
+    """ShallowStateTensor: shallow state tensor based on the QAOA circuit"""
+
+    def __init__(self, bond_dim, βγs):
+        self.βγs = βγs
+        self.p = len(βγs)
+        self.n_qubits = int(log2(bond_dim)) + 1
+
+    def num_qubits(self):
+        return self.n_qubits
+
+    def _decompose_(self, qubits):
+        return [[cirq.X(qubit) ** β for qubit in qubits] + \
+                [cirq.ZZ(qubits[i], qubits[i + 1]) ** γ for i in range(self.n_qubits - 1)]
+                for β, γ in split_2s(self.βγs)]
+
+    def _circuit_diagram_info_(self, args):
+        return ['U'] * self.n_qubits
+
+
+class ShallowEnvironment(cirq.Gate):
+    """ShallowEnvironmentTensor: shallow environment tensor based on the QAOA circuit"""
+
+    def __init__(self, bond_dim, βγs):
+        self.βγs = βγs
+        self.p = len(βγs)
+        self.n_qubits = 2 * int(log2(bond_dim))
+
+    def num_qubits(self):
+        return self.n_qubits
+
+    def _decompose_(self, qubits):
+        return [[cirq.X(qubit) ** β for qubit in qubits] +
+                [cirq.ZZ(qubits[i], qubits[i + 1]) ** γ for i in range(self.n_qubits - 1)]
+                for β, γ in split_2s(self.βγs)]
+
+    def _circuit_diagram_info_(self, args):
+        return ['V'] * self.n_qubits
+
+
+class Tensor(cirq.Gate):
+    def __init__(self, unitary, symbol):
+        self.U = unitary
+        self.n_qubits = int(log2(unitary.shape[0]))
+        self.symbol = symbol
+
+    def _unitary_(self):
+        return self.U
+
+    def num_qubits(self):
+        return self.n_qubits
+
+    def _circuit_diagram_info_(self, args):
+        return [self.symbol] * self.n_qubits
+
+    def __pow__(self, power, modulo=None):
+        if power == -1:
+            return self.__class__(self.U.conj().T, symbol=self.symbol + '†')
+        else:
+            return self.__class__(np.linalg.multi_dot([self.U] * power))
+
+
+class FullStateTensor(Tensor):
+    """StateTensor: represent state tensor as a unitary"""
+
+    def __init__(self, unitary, symbol='U'):
+        super().__init__(unitary, symbol)
+
+
+class FullEnvironment(Tensor):
+    """Environment: represents the environment tensor as a unitary"""
+
+    def __init__(self, unitary, symbol='V'):
+        super().__init__(unitary, symbol)
+
+
+class HorizontalSwapTest(cirq.Gate):
+    def __init__(self, u: cirq.Gate, v: cirq.Gate, bond_dim: int):
+        self.U = u
+        self.V = v
+        self.state = State(self.U, self.V, 1)
+        self.bond_dim = bond_dim
+
+    def num_qubits(self):
+        return self.state.num_qubits() + self.V.num_qubits()
+
+    def _decompose_(self, qubits):
+        state_qbs = self.state.num_qubits()
+        env_qbs = self.V.num_qubits()
+        target_qbs = range(int(log2(self.bond_dim)))
+
+        cnots = [cirq.CNOT(qubits[i], qubits[i+state_qbs]) for i in target_qbs]
+        hadamards = [cirq.H(qubits[i]) for i in target_qbs]
+        return [self.state._decompose_(qubits[:state_qbs]), self.V(*qubits[state_qbs:state_qbs+env_qbs])] +\
+            cnots + hadamards
+
+
+class VerticalSwapOptimizer(Optimizer):
+
+    def objective_function(self, v_params):
+        trial_environment = ShallowEnvironment(self.bond_dim, v_params)
+        state = State(self.u, trial_environment, 1)
+
+        qubits = cirq_qubits(state.num_qubits())
+        aux_qubits = int(self.v.num_qubits() / 2)
+        circuit = cirq.Circuit.from_ops([state.on(*qubits),
+                                         cirq.inverse(trial_environment).on(*(qubits[:aux_qubits] +
+                                                                              qubits[-aux_qubits:]))])
+        self.circuit = circuit
+
+        simulator = cirq.Simulator()
+        results = simulator.simulate(circuit)
+
+        target_qubits = self.get_target_qubits(aux_qubits, state.num_qubits())
+        prob_zeros = sum(np.absolute(results.final_simulator_state.state_vector[:int(target_qubits)])**2)
+        return 1-prob_zeros
+
+    @staticmethod
+    def get_target_qubits(aux_qubits, num_qubits):
+        other_qbs = num_qubits - aux_qubits
+        return 2**other_qbs - 1
+
+    def update_final_circuits(self):
+        v_params = self.optimized_result.x
+        self.v = ShallowEnvironment(self.bond_dim, v_params)
+
+
+class VerticalSampleSwapOptimizer(Optimizer):
+    def __init__(self, u_original: cirq.Gate, v_original: cirq.Gate, objective_function: Callable, reps: int, **kwargs):
+        super().__init__(u_original, v_original, objective_function, **kwargs)
+        self.reps = reps
+
+    def objective_function(self, params):
+        trial_environment = ShallowEnvironment(self.bond_dim, params)
+        state = State(self.u, trial_environment, 1)
+
+        qubits = cirq_qubits(state.num_qubits())
+        aux_qubs = int(self.v.num_qubits() / 2)
+        circuit = cirq.Circuit.from_ops([state.on(*qubits),
+                                         cirq.inverse(trial_environment).on(*(qubits[:aux_qubs] + qubits[-aux_qubs:])),
+                                         cirq.measure(*qubits[:aux_qubs])])
+
+        self.circuit = circuit
+        simulator = cirq.Simulator()
+        result = simulator.run(circuit, repetitions=self.reps)
+
+        key = list(result.measurements.keys())[0]
+        counter = result.histogram(key=key, fold_func=lambda e: sum(e))  # sum up the number of 1s in the measurements
+        mean = sum(counter.elements()) / self.reps
+        return mean
+
+
+class HorizontalSwapOptimizer(Optimizer):
+    def __init__(self, u_original: cirq.Gate, v_original: cirq.Gate, objective_function: Callable, reps: int, **kwargs):
+        super().__init__(u_original, v_original, objective_function, **kwargs)
+        self.reps = reps
+
+    def objective_function(self, params):
+        trial_environment = ShallowEnvironment(self.bond_dim, params)
+        state = State(self.u, trial_environment, 1)
+        horizontal_swap_gates = HorizontalSwapTest(self.u, trial_environment, self.bond_dim)
+
+        state_qubits = state.num_qubits()
+        env_qubits = trial_environment.num_qubits()
+        total_qubits = state_qubits+env_qubits
+        aux_qubs = int(env_qubits/2)
+        qubits = cirq_qubits(total_qubits)
+
+        circuit = cirq.Circuit.from_ops([horizontal_swap_gates(*qubits),
+                                         cirq.measure(qubits[:aux_qubs] + qubits[state_qubits:state_qubits+aux_qubs])])
+        self.circuit = circuit
+
+        simulator = cirq.Simulator()
+        results = simulator.run(circuit, repetitions=self.reps)
+        key = list(results.measurements.keys())[0]
+        counter = results.histogram(key=key)
+
+        measure_qubits = 2*aux_qubs
+        all_ones = int((2**measure_qubits)-1)
+        prob_all_ones = counter[all_ones]/self.reps
+        return prob_all_ones
+
+
--- qmps/tools.py
+++ qmps/tools.py
@@ -1,10 +1,17 @@
 from numpy import eye, concatenate, allclose, swapaxes, tensordot
 from numpy import array
-
+from numpy import zeros, block
 from math import log as mlog
 def log2(x): return mlog(x, 2)
-
+from scipy.linalg import norm
 from scipy.linalg import null_space
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import minimize
+import os
+from typing import Callable, List, Dict
+import cirq
+
 
 def cT(tensor):
     """H: Hermitian conjugate of last two indices of a tensor
@@ -13,11 +20,13 @@ def cT(tensor):
     """
     return swapaxes(tensor.conj(), -1, -2)
 
+
 def direct_sum(A, B):
     '''direct sum of two matrices'''
     (a1, a2), (b1, b2) = A.shape, B.shape
     O = zeros((a2, b1))
     return block([[A, O], [O.T, B]])
+
 
 def unitary_extension(Q, D=None):
     '''extend an isometry to a unitary (doesn't check its an isometry)'''
@@ -39,6 +48,7 @@ def unitary_extension(Q, D=None):
 
     return Q_
 
+
 def environment_to_unitary(v):
     '''put matrix in form
               ↑ ↑
@@ -51,6 +61,7 @@ def environment_to_unitary(v):
     v = v.reshape(1, -1)/norm(v)
     vs = null_space(v).conj().T
     return concatenate([v, vs], 0).T
+
 
 def tensor_to_unitary(A, testing=False):
     """given a left isometric tensor A, put into a unitary.
@@ -79,5 +90,87 @@ def tensor_to_unitary(A, testing=False):
 
     return U
 
+
 def unitary_to_tensor(U):
     return tensordot(U.reshape(*2*int(log2(U.shape[0]))*[2]), array([1, 0]), [2, 0]).transpose([1, 0, 2])
+
+
+def cirq_qubits(num):
+    return cirq.LineQubit.range(num)
+
+
+def split_2s(x):
+    """split_2s: take a list: [β, γ, β, γ, ...], return [[β, γ], [β, γ], ...]
+    """
+    return [x[i:i+2] for i in range(len(x)) if not i%2]
+
+
+class Optimizer:
+    '''
+    Base class for optimizers. To specify a new optimization technique simply define a new objective function
+    '''
+    def __init__(self, u_original: cirq.Gate, v_original: cirq.Gate,
+                 objective_function: Callable, qaoa_depth: int = 1,
+                 initial_guess: List = None, settings: Dict = None):
+        self.u = u_original
+        self.v = v_original
+        self.obj_fun = objective_function
+        self.iters = 0
+        self.obj_fun_values = []
+        self.store_values = False
+        self.optimized_result = None
+        self.circuit = None
+        self.initial_guess = initial_guess if initial_guess else np.random.random(2*qaoa_depth)
+        self.bond_dim = 2**(self.u.num_qubits()-1)
+
+        self._settings_ = settings if settings else{
+                                            'maxiter': 100,
+                                            'verbose': False,
+                                            'method': 'Nelder-Mead',
+                                            'tol': 1e-5,
+                                            'store_values': False
+                                            }
+
+    def settings(self, new_settings):
+        self._settings_.update(new_settings)
+
+    def callback_store_values(self, xk):
+        val = self.objective_function(xk)
+        self.obj_fun_values.append(val)
+        if self._settings_['verbose']:
+            print(f'{self.iters}:{val}')
+        self.iters += 1
+
+    def objective_function(self, params):
+        pass
+
+    def get_env(self):
+        options = {'maxiter': self._settings_['maxiter'],
+                   'disp': self._settings_['verbose']}
+
+        kwargs = {'fun': self.objective_function,
+                  'x0': self.initial_guess,
+                  'method': self.setting['method'],
+                  'tol': self._settings_['tol'],
+                  'options': options,
+                  'callback': self.callback_store_values if self._settings_['store_values'] else None}
+
+        self.optimized_result = minimize(**kwargs)
+        self.update_final_circuits()
+        if self._settings_['verbose']:
+            print(f'Reason for termination is {self.optimized_result.message}')
+
+    def plot_convergence(self, file):
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        plt.figure()
+        x = range(len(self.obj_fun_values))
+        plt.plot(x, self.obj_fun_values)
+        plt.xlabel('iterations')
+        plt.ylabel('objective function value')
+        plt.show()
+        if file:
+            plt.savefig(dir_path + '/' + file)
+
+    def update_final_circuits(self):
+        pass
+
