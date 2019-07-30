5da694b015f7e0e822bf64f5db0ce186cfc7d173
James Dborin <james.dborin.18@ucl.ac.uk>
2019-07-30 12:41:34 +0100

    started adding unitary tests but need to update master with new time evolve py

--- tests/test_time_evolve.py
+++ tests/test_time_evolve.py
@@ -2,21 +2,26 @@ import unittest
 import numpy as np
 from xmps.spin import spins
 from xmps.iMPS import iMPS
+from scipy.linalg import expm
+from ..qmps.represent import Tensor
+from ..qmps.time_evolve import
 import matplotlib.pyplot as plt
 
 Sx, Sy, Sz = spins(0.5)
+
 
 class TestTimeEvolve(unittest.TestCase):
     def setUp(self):
         N = 3  
         self.As = [iMPS().random(2, 2).mixed() for _ in range(N)]
+
     def test_time_evolve(self):
         for AL, AR, C in [self.As[0]]:
             J, g = -1, 0.5
-            H =  np.array([[J,g/2,g/2,0], 
-                           [g/2,-J,0,g/2], 
-                           [g/2,0,-J,g/2], 
-                           [0,g/2,g/2,J]] )
+            H = np.array([[J, g/2, g/2, 0],
+                          [g/2, -J, 0, g/2],
+                          [g/2, 0, -J, g/2],
+                          [0, g/2, g/2, J]])
 
 
             T = np.linspace(0, 5, 2000)
@@ -34,7 +39,10 @@ class TestTimeEvolve(unittest.TestCase):
             ax[0].plot(np.array(evs))
             ax[1].plot(es)
             plt.show()
+            '''
+            Below is the cirq time evolution. Gate is made using scipy.expm(H) * dt which is put into a Tensor, and 
+            and this is the hamiltonian.
+            '''
 
-
-if __name__=='__main__':
+if __name__ == '__main__':
     unittest.main(verbosity=1)
