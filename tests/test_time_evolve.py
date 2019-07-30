e590cbba168f97818562c58c46e2a0b373137e46
James Dborin <james.dborin.18@ucl.ac.uk>
2019-07-30 12:44:54 +0100

    adding Fergus code into test_time evolve

---
+++ tests/test_time_evolve.py
@@ -0,0 +1,33 @@
+class TestTimeEvolve(unittest.TestCase):
+    def setUp(self):
+        N = 3
+        self.As = [iMPS().random(2, 2).mixed() for _ in range(N)]
+
+    def test_time_evolve(self):
+        for AL, AR, C in [self.As[0]]:
+            J, g = -1, 0.5
+            H = np.array([[J, g/2, g/2, 0],
+                          [g/2, -J, 0, g/2],
+                          [g/2, 0, -J, g/2],
+                          [0, g/2, g/2, J]])
+
+
+            T = np.linspace(0, 5, 2000)
+            dt = T[1]-T[0]
+            evs = []
+            es = []
+            A = AL
+            for _ in T:
+                dA = A.dA_dt([H])*dt
+                es.append(A.e)
+                A = (A+dA).left_canonicalise()
+                evs.append(A.Es([Sx, Sy, Sz]))
+
+            fig, ax = plt.subplots(2, 1, sharex=True)
+            ax[0].plot(np.array(evs))
+            ax[1].plot(es)
+            plt.show()
+            '''
+            Below is the cirq time evolution. Gate is made using scipy.expm(H) * dt which is put into a Tensor, and 
+            and this is the hamiltonian.
+            '''
